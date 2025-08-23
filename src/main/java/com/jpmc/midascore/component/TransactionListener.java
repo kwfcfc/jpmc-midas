21080c06e29d8621501ca128111407690962cafb
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2025-08-23 03:45:46 +0000

    fix: improve task two
    1. configure the trusted package to deserialize
    2. use json deserializer

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,17 +1,21 @@
 package com.jpmc.midascore.component;
 
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.jpmc.midascore.foundation.Transaction;
 
 @Component
 public class TransactionListener {
     private static final Logger log = LoggerFactory.getLogger(TransactionListener.class);
 
     @KafkaListener(groupId = "${general.kafka-group-id}", topics = "${general.kafka-topic}")
-    public void listen(String record) {
-        log.info("Got '{}'", record);
+    public void listen(ConsumerRecord<String, Transaction> record) {
+        Transaction tx = record.value();
+        log.info("Got '{}'", tx.toString());
     }
 }
